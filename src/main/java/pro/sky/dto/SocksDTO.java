9900680ae4bd4615258dcebcbe7ec449e94872c7
klikli19 <103356373+klikli19@users.noreply.github.com>
2023-08-07 22:03:26 +0300

    add JavaDoc

--- src/main/java/pro/sky/dto/SocksDTO.java
+++ src/main/java/pro/sky/dto/SocksDTO.java
@@ -7,13 +7,25 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
 
+/**
+ * ДТО класс
+ */
 @Data
 @AllArgsConstructor
 public class SocksDTO {
+    /**
+     * Цвет носков
+     */
     private String color;
+    /**
+     * Процент хлопка, не должен превышать 100
+     */
     @Positive
     @Max(value = 100, message = "Процент хлопка в составе должен быть не больше 100%")
     private Integer cottonPart;
+    /**
+     * Количество пар, не должно быть меньше 1 на складе
+     */
     @Min(value = 1, message = "Количество пар носков должно быть не меньше 1")
     private Integer quantity;
 
--- src/main/java/pro/sky/mapper/SocksMapper.java
+++ src/main/java/pro/sky/mapper/SocksMapper.java
@@ -5,6 +5,9 @@ import org.mapstruct.ReportingPolicy;
 import pro.sky.dto.SocksDTO;
 import pro.sky.model.Sock;
 
+/**
+ * Класс для преобразования сущности в ДТО и наоборот
+ */
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SocksMapper {
     SocksDTO toDto(Sock sock);
